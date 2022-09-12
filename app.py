375129ebf39be6dea766e84210b8132873c2e1de
Tarabanov Serhii <tsv198250@gmail.com>
2022-09-12 20:42:20 +0300

    add rds
    add

--- app.py
+++ app.py
@@ -2,14 +2,22 @@
 
 import aws_cdk as cdk
 
+import tsv_ecs.rds_stack
 from tsv_ecs.tsv_ecs_stack import TsvEcsStack
 from tsv_ecs.pipeline_stack import PipelineStack
 from tsv_ecs.rds_stack import RdsStack
+from tsv_ecs.networking_stack import NetworkingStack
+
+db_creds_arn = "arn:aws:secretsmanager:eu-central-1:749874650085:secret:rdsCreds-Cs7lQ3"
 
 app = cdk.App()
-ecs_stack = TsvEcsStack(app, "TsvEcsStack", )
+networking_stack = NetworkingStack(app, "Networking")
+rds_stack = RdsStack(app, "RdsStack", vpc=networking_stack.vpc, creds_arn=db_creds_arn)
+ecs_stack = TsvEcsStack(app, "TsvEcsStack", vpc=networking_stack.vpc, db_secret=rds_stack.db_credentials)
 pipeline_stack = PipelineStack(app, "PipelineStack", service=ecs_stack.service)
-rds_stack = RdsStack(app, "RdsStack", )
+
+
+
 
 
 app.synth()
--- tsv_ecs/rds_stack.py
+++ tsv_ecs/rds_stack.py
@@ -2,37 +2,32 @@ from aws_cdk import (
     Stack,
     aws_rds as rds,
     aws_ec2 as ec2,
-    aws_codebuild as codebuild,
-    aws_codepipeline as codepipeline,
-    aws_codepipeline_actions as actions,
-    aws_iam as iam, Duration
-
+    aws_secretsmanager as secrets,
 )
 
 from constructs import Construct
-from werkzeug.debug import console
-
 
 class RdsStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, vpc: ec2.Vpc, creds_arn: str,  **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # vpc = ec2.Vpc(self, "tsv-bd", cidr="10.0.0.0/22", max_azs=3)
-        # credentials = rds.Credentials.from_generated_secret("clusteradmin")
-        #
-        # cluster = rds.DatabaseCluster(self, "Database", engine=rds.DatabaseClusterEngine.aurora_mysql(
-        #                               version=rds.AuroraMysqlEngineVersion.of(aurora_mysql_full_version="5.7.mysql_aurora.2.08.1")),
-        #                               credentials=credentials,
-        #                               # Optional - will default to 'admin' username and generated password
-        #                               instance_props=rds.InstanceProps(
-        #                               # optional , defaults to t3.medium
-        #                               # instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
-        #                               vpc=vpc,
-        #                               #vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
-        #                               publicly_accessible=True
-        #                               )
-        #                               )
+        self.db_credentials = secrets.Secret.from_secret_complete_arn(self, "creds", secret_complete_arn=creds_arn)
+        self.credentials = rds.Credentials.from_secret(self.db_credentials)
 
 
+        cluster = rds.DatabaseCluster(self, "Database", engine=rds.DatabaseClusterEngine.aurora_postgres(
+                                      version=rds.AuroraPostgresEngineVersion.VER_11_16),
+                                      # AuroraMysqlEngineVersion.of(aurora_mysql_full_version="5.7.mysql_aurora.2.08.1")),
+                                      credentials=self.credentials,
+                                      # Optional - will default to 'admin' username and generated password
+                                      instance_props=rds.InstanceProps(
+                                          # optional , defaults to t3.medium
+                                          instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3,
+                                                                            ec2.InstanceSize.MEDIUM),
+                                          vpc=vpc,
+                                          vpc_subnets=ec2.SubnetSelection(
+                                             subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT),
+                                      )
+                                      )
 

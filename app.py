c7e46eadd8f25feee8dd94bbf1508987f261cd0d
Tarabanov Serhii <tsv198250@gmail.com>
2022-09-29 00:29:25 +0300

    v-0032

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from tsv_ecs.rds_stack import RdsStack
 from tsv_ecs.networking_stack import NetworkingStack
 
 db_creds_arn = "arn:aws:secretsmanager:eu-central-1:571847562388:secret:secretDB-k7uD0M"
-#dfs
+
 app = cdk.App()
 networking_stack = NetworkingStack(app, "Networking")
 rds_stack = RdsStack(app, "RdsStack", vpc=networking_stack.vpc, creds_arn=db_creds_arn)

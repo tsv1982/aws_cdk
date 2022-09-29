ae3310ec3f3f36c7bd34e2919b7a04e858d64d7f
tsv1982 <tsv198250@gmail.com>
2022-09-29 13:53:43 +0300

    add class pipeline

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
-
 import tsv_ecs.rds_stack
 from tsv_ecs.tsv_ecs_stack import TsvEcsStack
 from tsv_ecs.pipeline_stack import PipelineStack
 from tsv_ecs.rds_stack import RdsStack
 from tsv_ecs.networking_stack import NetworkingStack
+from tsv_ecs.CdkPipilene import CdkPipeline
 
-db_creds_arn = "arn:aws:secretsmanager:eu-central-1:571847562388:secret:secretDB-k7uD0M"
+#db_creds_arn = "arn:aws:secretsmanager:eu-central-1:571847562388:secret:secretDB-k7uD0M"
 
 app = cdk.App()
-networking_stack = NetworkingStack(app, "Networking")
-rds_stack = RdsStack(app, "RdsStack", vpc=networking_stack.vpc, creds_arn=db_creds_arn)
-ecs_stack = TsvEcsStack(app, "TsvEcsStack", vpc=networking_stack.vpc, db_secret=rds_stack.db_credentials)
-pipeline_stack = PipelineStack(app, "PipelineStack", service=ecs_stack.service)
-
+#networking_stack = NetworkingStack(app, "Networking")
+#rds_stack = RdsStack(app, "RdsStack", vpc=networking_stack.vpc, creds_arn=db_creds_arn)
+#ecs_stack = TsvEcsStack(app, "TsvEcsStack", vpc=networking_stack.vpc, db_secret=rds_stack.db_credentials)
+#pipeline_stack = PipelineStack(app, "PipelineStack", service=ecs_stack.service)
+ckd_pipeline = CdkPipeline(app, "CdkPipeline")
 
 
 

1c52f79d590151a7f2079d389c3ea1f5b9917104
Tarabanov Serhii <tsv198250@gmail.com>
2022-10-16 11:31:52 +0300

    Update app.py
    Co-authored-by: Yan <yanvk@me.com>

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ from tsv_ecs.rds_stack import RdsStack
 from tsv_ecs.networking_stack import NetworkingStack
 from tsv_ecs.CdkPipilene import CdkPipeline
 
-#db_creds_arn = "arn:aws:secretsmanager:eu-central-1:571847562388:secret:secretDB-k7uD0M"
-
 app = cdk.App()
 #networking_stack = NetworkingStack(app, "Networking")
 #rds_stack = RdsStack(app, "RdsStack", vpc=networking_stack.vpc, creds_arn=db_creds_arn)

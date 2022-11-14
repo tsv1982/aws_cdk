b85d3768e21509f355634766e8cf93e1237d82e3
tsv1982 <tsv198250@gmail.com>
2022-11-14 17:33:47 +0200

    add infra branch

--- infra/lib/pipeline_stack.py
+++ infra/lib/pipeline_stack.py
@@ -25,10 +25,10 @@ class PipelineStack(Stack):
                                                       "phases": {
                                                           "pre_build": {
                                                               "commands": [
-                                                                  'REPOSITORY_URI=571847562388.dkr.ecr.eu-central-1.amazonaws.com/.venv-hello',
+                                                                  'REPOSITORY_URI=090146717911.dkr.ecr.eu-central-1.amazonaws.com/tsv-hello',
                                                                   'COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)',
                                                                   'IMAGE_TAG=${COMMIT_HASH:=latest}',
-                                                                  "aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 571847562388.dkr.ecr.eu-central-1.amazonaws.com"
+                                                                  "aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 090146717911.dkr.ecr.eu-central-1.amazonaws.com"
                                                               ]
                                                           },
                                                           "build": {"commands": [
